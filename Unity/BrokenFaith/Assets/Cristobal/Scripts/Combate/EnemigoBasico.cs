1bfa9608d1b0265d4acd45d71314738541ad6254
Crisstxt <cristobal.meneses.ojeda@gmail.com>
2024-02-13 14:28:31 +0100

    Script de enemigo

--- Unity/BrokenFaith/Assets/Cristobal/Scripts/Combate/EnemigoBasico.cs
+++ Unity/BrokenFaith/Assets/Cristobal/Scripts/Combate/EnemigoBasico.cs
@@ -10,6 +10,10 @@ public class EnemigoBasico : MonoBehaviour
     [SerializeField] private float velocidadMovimiento = 10f;        
     [SerializeField] private float distanciaAtaque = 0f;
 
+    [SerializeField] List<Vector3> puntos;
+    private Vector3 posIncial;
+    private int index;
+
     private Animator animator;
     private FovEnemigo fovEnemigo;
 
@@ -22,19 +26,34 @@ public class EnemigoBasico : MonoBehaviour
         animator = GetComponent<Animator>();
         fovEnemigo = GetComponent<FovEnemigo>();
         jugador = jugador = GameObject.FindGameObjectWithTag("Player");
+        posIncial = puntos[0];
     }
 
     void Update()
     {
-        if (fovEnemigo.GetDetectado())
-        {
-            PerseguirJugador();
-        }
+        if (fovEnemigo.GetDetectado()) PerseguirJugador();
+        else Patrullar();
     }
 
     private void PerseguirJugador()
     {
       transform.position = Vector3.MoveTowards(transform.position,jugador.transform.position, velocidadMovimiento * Time.deltaTime);
+    }
+
+    private void Patrullar()
+    {
+        if(puntos != null)
+        {
+            index = 0;
+
+            if (index > 0)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, jugador.transform.position, velocidadMovimiento * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, puntos[index]) < 0.1f) index = (index + 1) % puntos.Count;
+            }
+            else transform.position = Vector3.MoveTowards(transform.position, posIncial, velocidadMovimiento * Time.deltaTime);
+        }
     }
 
     private void Atacar()
