020c4ab1912a2a37435a25f10f6154b760eb3382
Crisstxt <99911916+Crisstxt@users.noreply.github.com>
2024-05-23 18:01:50 +0200

    Fix: Sonido Y prefabs

--- Unity/BrokenFaith/Assets/Scripts/Combate/IniciarMusica.cs
+++ Unity/BrokenFaith/Assets/Scripts/Combate/IniciarMusica.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class IniciarMusica : MonoBehaviour
 {
     [SerializeField] private Camera camaraMain;
+    [SerializeField] private AudioClip clipMusica;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            camaraMain.GetComponent<AudioSource>().loop = true;
+            camaraMain.GetComponent<AudioSource>().clip = clipMusica;
             camaraMain.GetComponent<AudioSource>().Play();
             this.enabled = false;
         }
